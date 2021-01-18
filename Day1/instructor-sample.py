1ee2a155aff27c2f4ce6cc5f0bcdc6b8bac2f0d4
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-18 17:14:39 +0800

    add Variable and Data Type sample

---
+++ Day1/instructor-sample.py
@@ -0,0 +1,9 @@
+# Variable and Data Type
+a = 5 # a is  a variable under the integer Date Type 
+b = "john" # b is  a variable under the String Date Type 
+c = 1.25 # c is  a variable under the float Date Type 
+d = True # d is a variable under the boolen Date Type 
+
+print( a ,"\n", b,"\n",c ,"\n",d ,"\n",)
+
+# "" and '' are the same for the Data Type
