bdb3a47aa8a743e66d072d493dda9bfd9a2c77f5
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-19 17:16:16 +0800

    adding string and string place holder using F or f

---
+++ Day2/instructor-sample2.py
@@ -0,0 +1,9 @@
+# Strings and String PLace Holder using F or f
+
+item = 'key'
+cost = 100.00
+quantity = 5
+
+sampleText5 = f"The product {item} cost {cost * quantity : .2f}  pesos"
+
+print(sampleText5)
