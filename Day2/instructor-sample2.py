90eb911f9138753bdbc3d39b7ee6822ab7c5a210
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-19 17:33:47 +0800

    adding modification

--- Day2/instructor-sample2.py
+++ Day2/instructor-sample2.py
@@ -4,6 +4,6 @@ item = 'key'
 cost = 100.00
 quantity = 5
 
-sampleText5 = f"The product {item} cost {cost * quantity : .2f}  pesos"
+sampleText5 = f"The product {item} cost {cost * quantity : .2f}  pesos" # useful for printing or displaying info Specially with computation
 
 print(sampleText5)
