4826aa4acde0423948120a7ba43ef85250884dae
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-19 17:15:12 +0800

    adding string and string place holder using %

---
+++ Day2/instructor-sample1.py
@@ -0,0 +1,10 @@
+# Strings and String PLace Holder using %
+
+item = 'key'
+cost = 100.00
+quantity = 5
+
+sampleText4 = "Product: %s cost: %.2f quantity: %i" %(item, cost, quantity)
+
+print(sampleText4)
+
