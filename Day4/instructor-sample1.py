00dab52616960513233b92789a994a61964c53c8
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-21 23:27:46 +0800

    adding Iteration looping (For loop using in) sample

---
+++ Day4/instructor-sample1.py
@@ -0,0 +1,8 @@
+#Iteration looping (For loop using in)
+
+names = ["Mike","Ana","Jun"]
+listval = [1,2,3,"ggg"]
+for name in names:
+    print(name)
+    #if name == "Mike":
+    #    print (f'{name} is good')
