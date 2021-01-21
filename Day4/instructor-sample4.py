c76191ce64ac386d04c119dba0543646acfd4674
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-21 23:32:52 +0800

    adding Interation looping (For loop using continue function) sample

---
+++ Day4/instructor-sample4.py
@@ -0,0 +1,5 @@
+##Iteration looping (For loop using continue function )
+for letter in "python python":
+    if letter == 't' or letter == 'h':
+        continue
+    print("Current Letter", letter)
