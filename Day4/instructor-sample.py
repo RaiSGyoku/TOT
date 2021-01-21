bf6580a5cc16dcc5d378dcb4a760a054355c28f3
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-21 23:26:30 +0800

    adding Iteration looping (while loop) sample

---
+++ Day4/instructor-sample.py
@@ -0,0 +1,13 @@
+#Iteration looping (while loop)
+
+a = 1 #declared
+
+while a < 6: #condition
+    #b=input('Enter') test
+    print(a)
+    a += 1 #  a = a + 1
+    if a == 2:
+        print(a)
+        a = 10 #loop terminator 
+    else :
+        print ('9')
