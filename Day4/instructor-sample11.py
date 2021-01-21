962046e95229237c18a1a41143fe17b5eda1513f
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-21 23:40:24 +0800

    adding count and index method for List / Array sample

---
+++ Day4/instructor-sample11.py
@@ -0,0 +1,10 @@
+# count and index method for list
+
+numlist = [25,44,22,13,25]
+counter = numlist.count(25)
+print(counter)
+
+
+numlist2 = [25,44,22,13,25]
+indexPos = numlist2.index(13)
+print(indexPos)
