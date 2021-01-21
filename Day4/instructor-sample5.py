0359a78a57ba2594730479e7ba63ecae6fb40185
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-21 23:30:41 +0800

    adding Iteration looping (For loop using pass function) sample

--- Day4/instructor-sample5.py
+++ Day4/instructor-sample5.py
@@ -4,5 +4,4 @@ for letter in "python":
         pass # pass is a place holder for the to be added feature
         print('this is the pass block')
     print("Current Letter", letter)
-
 print('goodbye')
