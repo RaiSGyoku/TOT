0708ff3484b61fadfdd9558120b9dd54b0ceadca
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-22 23:52:05 +0800

    adding Function with return sample

---
+++ Day5/instructor-sample3.py
@@ -0,0 +1,18 @@
+#Function with return
+def greet():
+    
+    return('hi ')
+
+print(greet())
+
+
+#Function with Parameters
+def solve(num1, num2):
+    sum = (num1+num2)
+    
+    return (f'sum is {sum}')
+
+
+
+
+print(solve(2,3))
