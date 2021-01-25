98f5244d3f0ade7b3a5e8d6b5d7b3853cd13b9b9
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:52:35 +0800

    adding activity 2 sub module

---
+++ Day6/Activity2_NetSalary.py
@@ -0,0 +1,7 @@
+# will be responsible for computing the net salary.
+
+def NetSalary(totalGross, totalDeduction):
+    
+    return (totalGross - totalDeduction )
+    
+#Tax(12% of the gross salary)
