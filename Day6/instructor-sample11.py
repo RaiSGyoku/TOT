b5e6e3f3ee7ce7dc82d8dba2b9357fbc374f76eb
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:49:10 +0800

    adding set methods ( remove and discard ) sample

---
+++ Day6/instructor-sample11.py
@@ -0,0 +1,21 @@
+# remove and discard Items SETS
+
+#remove   With the right value
+fruits={'Apple','Banana','santol'}
+fruits.remove('Banana') 
+print(fruits)
+
+#remove   
+fruits={'Apple','Banana','santol'}
+#fruits.remove('Guyabano') # throw some error code about this line
+print(fruits)
+
+#discard
+progs = {'C','C#','Java','PHP'}
+progs.discard('HTML') 
+print(progs)
+
+#discard With the right value
+progs = {'C','C#','Java','PHP'}
+progs.discard('Java') 
+print(progs)
