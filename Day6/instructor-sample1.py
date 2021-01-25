d27fca2ac3c961bf5c9d9be9912fe54b88f390c7
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:39:13 +0800

    adding tuple and function sample (len and max)

---
+++ Day6/instructor-sample1.py
@@ -0,0 +1,7 @@
+#Tuple and functions Sample (len and max)
+
+MyAnime = ('Naruto', 'Babidi', 'Doreamon','conan')
+print(len(MyAnime)) #count the item inside of the tuple
+
+myGrade = (89,90,92,89,87)
+print(max(myGrade)) #getting the highest value in the tuple
