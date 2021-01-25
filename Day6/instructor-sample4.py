3d5f0a73d22a6ffbdb4abf311a48187808f5b52e
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:42:46 +0800

    adding concatenating tuple sample

---
+++ Day6/instructor-sample4.py
@@ -0,0 +1,11 @@
+#concatenating tuple sample
+
+team7 = ('Naruto','Sasuke','Sakura')
+teamBa = ('Zoneo','Damulag','Babidi')
+
+teamAnime = (team7+teamBa)
+print(teamAnime)
+
+
+newteamAnime=sorted(teamAnime) # using short method
+print(newteamAnime)
