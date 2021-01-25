611ea2de9ce1e83a751c09925e180b465e85743f
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:49:52 +0800

    adding module sample

---
+++ Day6/instructor-sample12.py
@@ -0,0 +1,9 @@
+#module
+
+from instructor_sample12_1 import add, subtraction
+
+num1 = int(input("Enter 1st number: "))
+num2 = int(input("Enter 2nd number: "))
+
+print ('the sum is: ',add(num1,num2))
+print ('the subtraction is: ',subtraction(num1,num2))
