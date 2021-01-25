0d6cabaa9bfec637af8e356fb72fe646edc48d52
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-25 22:48:09 +0800

    adding set methods ( add and update) sample

---
+++ Day6/instructor-sample10.py
@@ -0,0 +1,11 @@
+# sets using add and update
+
+#add   
+fruits={'Apple','Banana','santol'}
+fruits.add('Guyabano')
+print(fruits)
+
+#update
+progs = {'C','C#','Java'}
+progs.update(['PHP','JS'])
+print(progs)
