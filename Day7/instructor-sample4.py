434cbb64be902d8bae6da556e3628df8dd1ea072
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-26 20:34:49 +0800

    adding Handing Exception sample3

---
+++ Day7/instructor-sample4.py
@@ -0,0 +1,13 @@
+# Handling Exceptions sample 3
+
+try:
+    result = 100/2
+except ZeroDivisionError:
+    print('Division by zero is error !!')
+except:
+    print("Wrong input")
+else:
+    #print("No Exceptions." , "Display the result: is: ", result)
+    print("No Exceptions." , f"Display the result: is: {result}")
+finally:
+    print("This will Execute no matter what")
