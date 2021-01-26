3094cfc2a11efce861a891b28fdf19ba430a4261
RaisGyoku <rais.hekigyoku@gmail.com>
2021-01-26 20:32:51 +0800

    adding random with dictionary using choice method sample

---
+++ Day7/instructor-sample1.py
@@ -0,0 +1,5 @@
+#random with dictionary using choice method
+import random
+names = ['Nobita','Naruto','Picolo','Asta','Santos','julien','jan','mark','jasper','calliope']
+
+print(random.choice(names))
