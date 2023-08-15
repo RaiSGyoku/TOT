865e62ee819ce0b10a0e1bd1c5aa2157eede46a4
RaiS <rais.hekigyoku@gmail.com>
2023-08-15 09:49:47 -0400

    removing instructor sample 1-3

--- Day2/instructor-sample2.py
+++
@@ -1,9 +0,0 @@
-# Strings and String PLace Holder using F or f
-
-item = 'key'
-cost = 100.00
-quantity = 5
-
-sampleText5 = f"The product {item} cost {cost * quantity : .2f}  pesos" # useful for printing or displaying info Specially with computation
-
-print(sampleText5)
