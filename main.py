dc9b8367b2c2f093a0acc305b0956bb704969cea
RaiS <rais.hekigyoku@gmail.com>
2023-08-16 02:23:03 -0400

    fixing displays

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def Day1():
     print('Float Sample: ',Variable_Data_Type_sample.float_sample())
     print('Boolen Sample: ',Variable_Data_Type_sample.bool_sample())
     
-    print('this are the sample of casting and operand: ')
+    print('\nthis are the sample of casting and operand: ')
     print('sum: ',Casting_and_Operand.sum('10','10','1.8'))
     print('diff: ',Casting_and_Operand.diff('10','10'))
     print('prod: ',Casting_and_Operand.prod('10','10'))
     print('qou: ',Casting_and_Operand.qou('10','10'))
 
-    print('Here is the sample for input function')
+    print('\nHere is the sample for input function')
     Input_Functions.Input_Sample()
 
 def Day2():
@@ -30,8 +30,14 @@ def Day2():
     grade1 = 95.50
     grade2 = 95.20
 
+
+
+
     #print(Format_String_Functions.length_Sample(Sample_Text1))
     #print(Strings_and_PLace_Holders.Input_and_Place_Holder())
+    
+    
+    print("Numeric Sample")
     print(f"sample of round function\n{ Numeric_Functions.Round_Sample(grade1)}\n")
     print(f"sample of math ceiling function\n{Numeric_Functions.Math_Ceiling_Sample(grade1,grade2)}\n" )
     print(f"sample of math floor function\n{ Numeric_Functions.Math_Floor_Sample(grade1,grade2)}\n")
@@ -39,7 +45,8 @@ def Day2():
     print(f"alternative of pow function\n{ Numeric_Functions.exponent_sample2()}\n")
 
 def main():
-
+    Day1()
+    print('\n')
     Day2()
     
 
