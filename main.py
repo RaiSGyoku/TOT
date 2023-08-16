5f41fed5b0bd1cf510724b3a3406daecad13bd09
RaiS <rais.hekigyoku@gmail.com>
2023-08-16 02:17:24 -0400

    adding numeric sample into main.py

--- main.py
+++ main.py
@@ -27,8 +27,16 @@ def Day2():
     Sample_Text1 = "JASPER MARK"
     Sample_Text2 = 'mark'
 
-    print(Format_String_Functions.length_Sample(Sample_Text1))
-    print(Strings_and_PLace_Holders.Input_and_Place_Holder())
+    grade1 = 95.50
+    grade2 = 95.20
+
+    #print(Format_String_Functions.length_Sample(Sample_Text1))
+    #print(Strings_and_PLace_Holders.Input_and_Place_Holder())
+    print(f"sample of round function\n{ Numeric_Functions.Round_Sample(grade1)}\n")
+    print(f"sample of math ceiling function\n{Numeric_Functions.Math_Ceiling_Sample(grade1,grade2)}\n" )
+    print(f"sample of math floor function\n{ Numeric_Functions.Math_Floor_Sample(grade1,grade2)}\n")
+    print(f"sample of pow function\n{ Numeric_Functions.exponent_sample1()}\n")
+    print(f"alternative of pow function\n{ Numeric_Functions.exponent_sample2()}\n")
 
 def main():
 
