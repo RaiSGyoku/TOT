7e1a49ba681135aa03f8eda992f39a64aafed890
RaiS <rais.hekigyoku@gmail.com>
2023-08-23 05:24:14 -0400

    fixing variable naming

--- Day3/Activities.py
+++ Day3/Activities.py
@@ -1,29 +1,29 @@
 def Activity1():
     Name = input('Enter Full Name: ')
-    MathGrade = float(input('Enter Math Grade: '))
-    ScienceGrade = float(input('Enter Science Grade: '))
-    EnglishGrade = float(input('Enter English Grade: '))
+    Math_Grade = float(input('Enter Math Grade: '))
+    Science_Grade = float(input('Enter Science Grade: '))
+    English_Grade = float(input('Enter English Grade: '))
 
-    Average = (float(MathGrade) + float(ScienceGrade) + float(EnglishGrade)) / 3
+    Average = (float(Math_Grade) + float(Science_Grade) + float(English_Grade)) / 3
 
     if Average >= 75:
-        if MathGrade <= 74 and EnglishGrade <= 74: 
+        if Math_Grade <= 74 and English_Grade <= 74: 
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll Math and English Subject")
-        elif MathGrade <= 74 and ScienceGrade <= 74:
+        elif Math_Grade <= 74 and Science_Grade <= 74:
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll Math and Science Subject")
-        elif EnglishGrade <= 74 and ScienceGrade <= 74:
+        elif English_Grade <= 74 and Science_Grade <= 74:
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll Science and English  Subject")
         
-        elif EnglishGrade <= 74:
+        elif English_Grade <= 74:
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll English Subject")
-        elif ScienceGrade <= 74:
+        elif Science_Grade <= 74:
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll Science Subject")
-        elif MathGrade <= 74:
+        elif Math_Grade <= 74:
             print(f'Average:  {Average : .2f}')
             print('Message: ' + "Congratuations! you Passed the Semester. But you need to re-enroll Math Subject")
 
