8c571b4968e9834c37b10dc354f7caf590a969c5
RaiS <rais.hekigyoku@gmail.com>
2023-09-06 04:17:47 -0400

    add new sample

--- Day5/instructor_samples.py
+++ Day5/instructor_samples.py
@@ -8,5 +8,14 @@ class Function_with_Parameters():
     def greet(name):
         print("hi " + name)
 
+class Function_with_default_value_parameter():
+    def greet(name = 'John Doe'):
+        print('hi '+ name)
+
+
+
+
 Defining_and_Calling_a_Function.greet() #greet() 
 Function_with_Parameters.greet('john')
+Function_with_default_value_parameter.greet()
+Function_with_default_value_parameter.greet('anna')
