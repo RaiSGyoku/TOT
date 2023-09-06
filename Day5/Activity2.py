9fa633be7f6a8f45ff35722fac8f8970c184c2ad
RaiS <rais.hekigyoku@gmail.com>
2023-09-06 04:30:29 -0400

    removing old Activity

--- Day5/Activity2.py
+++
@@ -1,6 +0,0 @@
-
-def reverseWord(inp):
-    backwards = inp[::-1]
-    return(f'original word: {inp} produced: {backwards} number of character: {len(inp)}')
-
-print(reverseWord( inp = input('Enter word here: ')))
