936890501421b562715213099999a6f77adec15b
RaiS <rais.hekigyoku@gmail.com>
2023-09-07 07:56:10 -0400

    create sample file and recreate tuple sample

---
+++ Day6/instructor_samples.py
@@ -0,0 +1,14 @@
+class Tuple():
+    def usng_len_Function():
+        MyAnime = ('Naruto', 'Babidi', 'Doreamon','conan')
+        print(len(MyAnime)) #count the item inside of the tuple
+
+    def usng_max_Function():
+        myGrade = (89,90,92,89,87)
+        print(max(myGrade)) #getting the highest value in the tuple
+
+class Dictionary():
+    pass
+
+Tuple.usng_len_Function()
+Tuple.usng_max_Function()
