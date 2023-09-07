003b6b6ef3f2fe27eb59a608441a57b64e78d56d
RaiS <rais.hekigyoku@gmail.com>
2023-09-07 08:25:58 -0400

    adding new sample

--- Day6/instructor_samples.py
+++ Day6/instructor_samples.py
@@ -47,7 +47,17 @@ class Tuple():
 
 
 class Dictionary():
-    pass
+    def baic_sample():
+        dict = {'Name': 'Cyrel', 'Age': 19, 'Class':'1st'}
+        dict['Age'] = 23 #update existing entry
+        dict['School'] = 'Gallanosa NHS' #update existing entry
+        
+        print('dict["Age"]:' ,dict['Age'])
+        print('dict["School"]:' ,dict['School'])
+
+#print(f"dict['Class']: {dict['Class']}" ) #can be use the format
 
 Tuple.usng_len_Function()
 Tuple.usng_max_Function()
+
+Dictionary.baic_sample()
