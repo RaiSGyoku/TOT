2fbebe098f1cbaea9f0780ca198f6668c6938467
RaiS <rais.hekigyoku@gmail.com>
2023-09-07 07:57:50 -0400

    add new sample

--- Day6/instructor_samples.py
+++ Day6/instructor_samples.py
@@ -7,6 +7,16 @@ class Tuple():
         myGrade = (89,90,92,89,87)
         print(max(myGrade)) #getting the highest value in the tuple
 
+    def basic_sample():
+        tup1 = ('physical','chemistry',1997,2000)
+        tup2 = (1,2,3,4,5,6,7)
+        
+        print("tup1[0]:", tup1[0])
+        print("tup2[1:5]:", tup2[1:5]) # name of tuple[index:range] 
+        
+        print(f"tup2[1:len(tup2): {tup2[1:len(tup2)]}") #can be use as well 
+
+
 class Dictionary():
     pass
 
