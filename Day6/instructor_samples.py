288cf6816e19e8e8b7c5661dfc93b52f9c877c53
RaiS <rais.hekigyoku@gmail.com>
2023-09-08 03:34:15 -0400

    add new sample

--- Day6/instructor_samples.py
+++ Day6/instructor_samples.py
@@ -57,7 +57,18 @@ class Dictionary():
 
 #print(f"dict['Class']: {dict['Class']}" ) #can be use the format
 
+    def update_Dictionary_value():
+        dict = {'Name': 'Cyrel', 'Age': 19, 'Class':'1st'}
+        dict['Age'] = 23 #update existing entry
+        dict['School'] = 'Gallanosa NHS' #update existing entry
+
+        print('dict["Age"]:' ,dict['Age'])
+        print('dict["School"]:' ,dict['School'])
+        #print(f"dict['Class']: {dict['Class']}" ) #can be use the format
+
+
 Tuple.usng_len_Function()
 Tuple.usng_max_Function()
 
 Dictionary.baic_sample()
+Dictionary.update_Dictionary_value()
