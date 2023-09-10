2a42413b229868035032c60f62a159518f7c1085
RaiS <rais.hekigyoku@gmail.com>
2023-09-10 05:09:20 -0400

    removing old activity

--- Day6/Activity2_GrossSalary.py
+++
@@ -1,5 +0,0 @@
-#will handle the function for computing the gross salary.
-
-def grossSalary(hours):
-    ratePerHour = 500 
-    return (ratePerHour * int(hours))
--- Day6/Activity2_NetSalary.py
+++
@@ -1,7 +0,0 @@
-# will be responsible for computing the net salary.
-
-def NetSalary(totalGross, totalDeduction):
-    
-    return (totalGross - totalDeduction )
-    
-#Tax(12% of the gross salary)
