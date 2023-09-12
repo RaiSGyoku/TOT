2da94b0f00f619ef4fd14915c8815af593738309
RaiS <rais.hekigyoku@gmail.com>
2023-09-12 11:00:21 -0400

    Activity 2 solution

--- Day7/Activities.py
+++ Day7/Activities.py
@@ -23,35 +23,33 @@ def Activity1():
     
 # ['jasper Santos Santos', 'Naruto jimenez mori', 'julien umali jesusa', 'jasper lee Dela Cruz']
 
-def contidtion_Test(inpChoose):
-    if int(inpChoose) == 0:
-        return(False)
-    else:
-        inpVal1 = int(input('Enter first number: '))
-        inpVal2 = int(input('Enter second number: '))
-        if int(inpChoose) == 1:
-            out = inpVal1 + inpVal2
-            print(f'output are: {out}')
-        elif int(inpChoose) == 2:
-            out = inpVal1 - inpVal2
-            print(f'output are: {out}')
-        elif int(inpChoose) == 3:
-            out = inpVal1 * inpVal2
-            print(f'output are: {out}')
-        elif int(inpChoose) == 4:
-            out = inpVal1 / inpVal2
-            print(f'output are: {out}')
+def calculator(chosen):
+    value_1 = int(input('Enter first number: '))
+    value_2 = int(input('Enter second number: '))
+    if int(chosen) == 1:
+        out = value_1 + value_2
+        return(f'output are: {out}')
+    elif int(chosen) == 2:
+        out = value_1 - value_2
+        return(f'output are: {out}')
+    elif int(chosen) == 3:
+        out = value_1 * value_2
+        return(f'output are: {out}')
+    elif int(chosen) == 4:
+        out = value_1 / value_2
+        return(f'output are: {out}')
 
 def Activity2():
     try:
-        inpChoose = input('Enter number 1-4\nno.1 for Addition\nno.2 for Subtration\nno.3 for Multiply\nno.4 for Division\nno 0 for exit: ')
-        while contidtion_Test(inpChoose) is True:
-           
-            contidtion_Test(inpChoose)
-
+        while True:
+            chosen = input('Enter number 1-4\nno.1 for Addition\nno.2 for Subtration\nno.3 for Multiply\nno.4 for Division\nno 0 for exit: ')
+            if int(chosen) != 0:
+                print(calculator(chosen))
+            else:
+                break
     except ZeroDivisionError:
         print('Division By Zero is Error !!')
-    #except :
-    #    print('Invalid input, Please Enter Numbers only')
+    except :
+        print('Invalid input, Please Enter Numbers only')
 
-Activity2() 
+Activity2()
