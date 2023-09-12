65f315c9a72c4636a511d4caa5d170448b74e390
RaiS <rais.hekigyoku@gmail.com>
2023-09-12 10:31:12 -0400

    semi fix

--- Day7/Activities.py
+++ Day7/Activities.py
@@ -21,30 +21,37 @@ def Activity1():
             print(new_list)
             break
     
-Activity1() # ['jasper Santos Santos', 'Naruto jimenez mori', 'julien umali jesusa', 'jasper lee Dela Cruz']
+# ['jasper Santos Santos', 'Naruto jimenez mori', 'julien umali jesusa', 'jasper lee Dela Cruz']
+
+def contidtion_Test(inpChoose):
+    if int(inpChoose) == 0:
+        return(False)
+    else:
+        inpVal1 = int(input('Enter first number: '))
+        inpVal2 = int(input('Enter second number: '))
+        if int(inpChoose) == 1:
+            out = inpVal1 + inpVal2
+            print(f'output are: {out}')
+        elif int(inpChoose) == 2:
+            out = inpVal1 - inpVal2
+            print(f'output are: {out}')
+        elif int(inpChoose) == 3:
+            out = inpVal1 * inpVal2
+            print(f'output are: {out}')
+        elif int(inpChoose) == 4:
+            out = inpVal1 / inpVal2
+            print(f'output are: {out}')
 
 def Activity2():
     try:
-        while True:
-            inpChoose = input('Enter number 1-4\nno.1 for Addition\nno.2 for Subtration\nno.3 for Multiply\nno.4 for Division\nno 0 for exit: ')
-            if int(inpChoose) == 0:
-                break
-            else:
-                inpVal1 = int(input('Enter first number: '))
-                inpVal2 = int(input('Enter second number: '))
-                if int(inpChoose) == 1:
-                    out = inpVal1 + inpVal2
-                    print(f'output are: {out}')
-                elif int(inpChoose) == 2:
-                    out = inpVal1 - inpVal2
-                    print(f'output are: {out}')
-                elif int(inpChoose) == 3:
-                    out = inpVal1 * inpVal2
-                    print(f'output are: {out}')
-                elif int(inpChoose) == 4:
-                    out = inpVal1 / inpVal2
-                    print(f'output are: {out}')
+        inpChoose = input('Enter number 1-4\nno.1 for Addition\nno.2 for Subtration\nno.3 for Multiply\nno.4 for Division\nno 0 for exit: ')
+        while contidtion_Test(inpChoose) is True:
+           
+            contidtion_Test(inpChoose)
+
     except ZeroDivisionError:
         print('Division By Zero is Error !!')
-    except :
-        print('Invalid input, Please Enter Numbers only')
+    #except :
+    #    print('Invalid input, Please Enter Numbers only')
+
+Activity2() 
