ccb9b25115571d3e5bbc27f6617668309239f2df
RaiS <rais.hekigyoku@gmail.com>
2023-09-13 03:02:22 -0400

    recreate sample

--- Day7/instructor_samples.py
+++ Day7/instructor_samples.py
@@ -31,3 +31,16 @@ class Handling_Exceptions():
             print("No Exceptions." , f"Display the result: is: {result}")
         finally:
             print("This will Execute no matter what")
+
+    def sample_2():
+        try:
+            result = 100/a
+        except ZeroDivisionError:
+            print('Division by zero is error !!')
+        except:
+            print("Wrong input")
+        else:
+            #print("No Exceptions." , "Display the result: is: ", result)
+            print("No Exceptions." , f"Display the result: is: {result}")
+        finally:
+            print("This will Execute no matter what")
