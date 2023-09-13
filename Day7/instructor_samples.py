8635aa0d3fa5df7568ac654e1a4ee8cc11875bb2
RaiS <rais.hekigyoku@gmail.com>
2023-09-13 01:06:33 -0400

    recreate sample

--- Day7/instructor_samples.py
+++ Day7/instructor_samples.py
@@ -7,28 +7,3 @@ class random_fucntion():
     def using_randint_function_with_for_loop(generated_random = random):
         for i in range(5):
             print(generated_random.randint(1,10))
-
-    def using_choice_for_int(generated_random = random):
-        print(generated_random.choice([10,20,30,40,50]))
-
-    def using_choice_for_str(generated_random = random):
-        print(generated_random.choice(['yes','no','maybe','nope']))
-    
-    def using_choice_with_dictionary(generated_random = random):
-        names = ['Nobita','Naruto','Picolo','Asta','Santos','julien','jan','mark','jasper','calliope']
-        print(generated_random.choice(names))
-
-class Handling_Exception():
-    def sample_1():
-        try:
-            result = 100/0
-        except ZeroDivisionError:
-            print('Division by zero is error !!')
-        except:
-            print("Wrong input")
-        else:
-            #print("No Exceptions." , "Display the result: is: ", result)
-            print("No Exceptions." , f"Display the result: is: {result}")
-        finally:
-            print("This will Execute no matter what")
-
