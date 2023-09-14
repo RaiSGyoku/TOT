e4d8ebfdf83de55069a78d7751395891374719c3
RaiS <rais.hekigyoku@gmail.com>
2023-09-14 01:44:56 -0400

    removing old sample

--- Day7/instructor-sample1.py
+++
@@ -1,5 +0,0 @@
-#random with dictionary using choice method
-import random
-names = ['Nobita','Naruto','Picolo','Asta','Santos','julien','jan','mark','jasper','calliope']
-
-print(random.choice(names))
--- Day7/instructor-sample5.py
+++
@@ -1,7 +0,0 @@
-# Handling Exceptions with external file sample
-try:
-    file = open("sample6.txt")
-except FileNotFoundError as fileError:
-    print(fileError)
-else:
-    print("file is Found!")
