c1eb790f9ffa604d35c42b6556d3d246d89d22c2
RaiS <rais.hekigyoku@gmail.com>
2023-09-18 11:47:45 -0400

    recreate activity

---
+++ Day9/Activity.py
@@ -0,0 +1,34 @@
+import os
+
+
+    
+class Activity():
+    try:
+        inResp = 'y'
+        while is_True(inResp):
+            chosen = input("Enter no.1 for Add Record\nEnter no.2 for view Records\nEnter no.3 for clear All Records\nEnter no.4 for Exit\n Input: ")
+            if int(chosen) == 1:
+                inpName = input("Enter name: ")
+                inpEmail = input("Enter Email: ")
+                inpAddress = input("Enter Address: ")
+
+                add(inpName,inpEmail,inpAddress)
+                print('Added')
+            
+            elif int(chosen) == 2:
+                view()
+
+            elif int(chosen) == 3:
+                delete()
+
+            elif int(chosen) == 4:
+                print("Thank You")
+                break
+            
+            inResp = input('wants to try again?, Press y if Yes if no press anykey: ')
+    
+
+    except:
+        print("invalid Input")
+
+Activity()
