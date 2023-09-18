a2dc339972815a140c472f0ed2e510b7c69fabe2
RaiS <rais.hekigyoku@gmail.com>
2023-09-18 10:44:56 -0400

    recreate sample

--- Day9/instructor_samples.py
+++ Day9/instructor_samples.py
@@ -8,6 +8,11 @@ def write_File():
     file = open("Day9/info.txt", "a")
     file.write("New Text")
     file.close()
-    
+
     file = open("Day9/info.txt", "r")
     print(file.read())
+
+def remove_file():
+    import os
+    
+    os.remove("Day9/info.txt")
